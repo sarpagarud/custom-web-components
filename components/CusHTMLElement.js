1650556cec626f9683d2869733dcd616b3f8a23a
sarpagarud <241446179+sarpagarud@users.noreply.github.com>
2026-07-21 15:34:36 +0530

    Create CusHTMLElement.js

---
+++ components/CusHTMLElement.js
@@ -0,0 +1,47 @@
+
+const CustomGlobalCss = `
+  span { font-size: 50px; }
+`;
+
+class CusHTMLElement extends HTMLElement {
+  #shadow = null;
+
+  constructor() {
+    super();
+    this.#shadow = this.attachShadow(
+      { mode: "open" }
+    );
+    this.#setMainCssSheet();
+  }
+
+  connectedCallback() {
+    console.log("Custom element added to page.");
+  }
+
+  disconnectedCallback() {
+    console.log("Custom element removed from page.");
+  }
+
+  connectedMoveCallback() {
+    console.log("Custom element moved with moveBefore()");
+  }
+
+  adoptedCallback() {
+    console.log("Custom element moved to new page.");
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute ${name} has changed.`);
+  }
+
+  #setMainCssSheet(){
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(CustomGlobalCss);
+    this.#shadow.adoptedStyleSheets.push(
+      sheet
+    );
+  }
+  setChild(child){
+    this.#shadow.appendChild(child);
+  }
+}
